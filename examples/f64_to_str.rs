9fe74fcc3ee8d6f61565f103d30833912e4343c9
Anurag <107351441+unknownK19@users.noreply.github.com>
2024-11-21 11:24:04 +0530

    add DebugODisplay for print statement or alternative, Improved Code, adding i64 and f64

--- examples/f64_to_str.rs
+++ examples/f64_to_str.rs
@@ -1,32 +1,32 @@
 use embedded_cotoutf8::{COtoUTF8, DebugODisplay};
 
 fn main() {
-    let num: f32 = 1.0005;
+    let num: f64 = 1.0005;
     let binding = num.coto_utf8();
     let result = core::str::from_utf8(&binding).unwrap();
     println!("{}", result);
 
-    let num: f32 = -123.0005;
+    let num: f64 = -123.0005;
     let binding = num.coto_utf8();
     let result = core::str::from_utf8(&binding).unwrap();
     println!("{}", result);
 
-    let num: f32 = 1524.001;
+    let num: f64 = 1524.001;
     let binding = num.coto_utf8();
     let result = core::str::from_utf8(&binding).unwrap();
     println!("{}", result);
 
-    let num: f32 = 44245.12;
+    let num: f64 = 44245.12;
     let binding = num.coto_utf8();
     let result = core::str::from_utf8(&binding).unwrap();
     println!("{}", result);
 
-    let num: f32 = 442400005.001;
+    let num: f64 = 442400005.001;
     let binding = num.coto_utf8();
     let result = core::str::from_utf8(&binding).unwrap();
     println!("{}", result);
 
     // Alternative use this
-    let num = DebugODisplay(44245.12f32);
+    let num = DebugODisplay(44245.12f64);
     println!("{}", num)
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,10 @@
 #![no_std]
 
+use core::{
+    f64,
+    fmt::{Debug, Display},
+};
+
 pub trait COtoUTF8<const O: usize> {
     fn coto_utf8(&self) -> [u8; O];
 }
@@ -14,13 +19,38 @@ impl COtoUTF8<3> for u8 {
         let n = *self;
 
         for (i, j) in digit.iter_mut().enumerate() {
-            *j = b"0"[0] + n / (10u8.pow(i as u32)) % 10
+            *j = b'0' + n / (10u8.pow(i as u32)) % 10
         }
 
         digit.reverse();
         digit
     }
 }
+
+pub struct DebugODisplay<T: COtoUTF8<O>, const O: usize>(pub T);
+
+impl<T, const O: usize> Debug for DebugODisplay<T, O>
+where
+    T: COtoUTF8<O>,
+{
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        f.write_str(core::str::from_utf8(&self.0.coto_utf8()).unwrap())
+            .unwrap();
+        Ok(())
+    }
+}
+
+impl<T, const O: usize> Display for DebugODisplay<T, O>
+where
+    T: COtoUTF8<O>,
+{
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        f.write_str(core::str::from_utf8(&self.0.coto_utf8()).unwrap())
+            .unwrap();
+        Ok(())
+    }
+}
+
 /**
 F32 (cap F 32) is another floating point in future. Unlike f32 IEEE 754-2008, it has less error but no precise as f32.
 so here's How it look
@@ -34,8 +64,14 @@ so here's How it look
 EEEE_EEEE MMMM_MMMM_MMMM_MMMM_MMMM_MMMM
 |---i8--| |--- i24 = i8 + u16---------|
 
-NOTE: Current WIP
+NOTE: Currently WIP
 */
+
+// impl Debug for F32 {
+//     fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+//
+//     }
+// }
 
 #[derive(Default)]
 pub struct F32(i8, i8, u16);
@@ -174,7 +210,7 @@ impl COtoHex<2> for u8 {
 
         for (i, j) in sample.iter_mut().enumerate() {
             *j = match (self >> (4 * i) & 0x0F) % 16 {
-                int @ 0..=9 => b"0"[0] + int,
+                int @ 0..=9 => b'0' + int,
                 alpha @ 10..=15 => 55 + alpha,
                 _ => 0,
             }
@@ -190,7 +226,7 @@ impl COtoHex<4> for u16 {
 
         for (i, j) in sample.iter_mut().enumerate() {
             *j = match ((self >> (4 * i) & 0x0F) % 16) as u8 {
-                int @ 0..=9 => b"0"[0] + int,
+                int @ 0..=9 => b'0' + int,
                 alpha @ 10..=15 => 55 + alpha,
                 _ => 0,
             }
@@ -206,7 +242,7 @@ impl COtoHex<8> for u32 {
 
         for (i, j) in sample.iter_mut().enumerate() {
             *j = match ((self >> (4 * i) & 0x0F) % 16) as u8 {
-                int @ 0..=9 => b"0"[0] + int,
+                int @ 0..=9 => b'0' + int,
                 alpha @ 10..=15 => 55 + alpha,
                 _ => 0,
             }
@@ -219,11 +255,7 @@ impl COtoHex<8> for u32 {
 impl COtoUTF8<9> for f32 {
     fn coto_utf8(&self) -> [u8; 9] {
         let mut buffer = [0u8; 9];
-        buffer[0] = if self.is_sign_negative() {
-            b"-"[0]
-        } else {
-            b" "[0]
-        };
+        buffer[0] = if self.is_sign_negative() { b'-' } else { b' ' };
         if self.is_infinite() {
             buffer[1..].copy_from_slice(b"INFINITY");
         } else {
@@ -240,14 +272,50 @@ impl COtoUTF8<9> for f32 {
             }
             if (-6..=3).contains(&exponential) {
                 let exponent = exponential.max(0) as usize;
-                buffer[2 + exponent] = b"."[0];
+                buffer[2 + exponent] = b'.';
                 let cotonum = ((self * 1_000_000.0) as i32).coto_utf8();
                 buffer[1..={ exponent + 1 }].copy_from_slice(&cotonum[{ 4 - exponent }..=4]);
                 buffer[{ 3 + exponent }..].copy_from_slice(&cotonum[{ 5 + exponent }..]);
             } else {
-                buffer[5..].copy_from_slice(&exponential.coto_utf8());
-                buffer[4] = b"E"[0];
+                buffer[6] = exponential.coto_utf8()[0].max(b'+');
+                buffer[7..].copy_from_slice(&exponential.coto_utf8()[2..]);
+                buffer[5] = b'E';
                 buffer[1..=3].copy_from_slice(&num.coto_utf8()[1..=3])
+            }
+        }
+        buffer
+    }
+}
+
+impl COtoUTF8<11> for f64 {
+    fn coto_utf8(&self) -> [u8; 11] {
+        let mut buffer = [0u8; 11];
+        buffer[0] = if self.is_sign_negative() { b'-' } else { b' ' };
+        if self.is_infinite() {
+            buffer[1..].copy_from_slice(b"INFINITY");
+        } else {
+            let mut num = if *self < 0. { -*self } else { *self };
+            let mut exponential: i16 = 0;
+            while !(0. ..=10.).contains(&num) {
+                if num < 0. {
+                    exponential -= 1;
+                    num *= 10.;
+                } else {
+                    exponential += 1;
+                    num /= 10.;
+                }
+            }
+            if (-8..=5).contains(&exponential) {
+                let exponent = exponential.max(0) as usize;
+                buffer[2 + exponent] = b'.';
+                let cotonum = ((self * 100_000_000.0) as i64).coto_utf8();
+                buffer[1..={ exponent + 1 }].copy_from_slice(&cotonum[{ 11 - exponent }..=11]);
+                buffer[{ 3 + exponent }..].copy_from_slice(&cotonum[{ 12 + exponent }..]);
+            } else {
+                buffer[7] = exponential.coto_utf8()[0].max(b'+');
+                buffer[8..].copy_from_slice(&exponential.coto_utf8()[3..]);
+                buffer[6] = b'E';
+                buffer[1..=5].copy_from_slice(&num.coto_utf8()[1..=5])
             }
         }
         buffer
@@ -257,11 +325,11 @@ impl COtoUTF8<9> for f32 {
 impl COtoUTF8<4> for i8 {
     fn coto_utf8(&self) -> [u8; 4] {
         let mut digit: [u8; 4] = [0; 4];
-        digit[0] = if self.is_negative() { b"-"[0] } else { b" "[0] };
+        digit[0] = if self.is_negative() { b'-' } else { b' ' };
         let n = self.abs();
 
         for (i, j) in digit[1..].iter_mut().enumerate() {
-            *j = (n / (10i8.pow(i as u32)) % 10) as u8 + b"0"[0]
+            *j = (n / (10i8.pow(i as u32)) % 10) as u8 + b'0'
         }
 
         digit[1..].reverse();
@@ -273,11 +341,11 @@ impl COtoUTF8<4> for i8 {
 impl COtoUTF8<6> for i16 {
     fn coto_utf8(&self) -> [u8; 6] {
         let mut digit: [u8; 6] = [0; 6];
-        digit[0] = if self.is_negative() { b"-"[0] } else { b" "[0] };
+        digit[0] = if self.is_negative() { b'-' } else { b' ' };
         let n = self.abs();
 
         for (i, j) in digit[1..].iter_mut().enumerate() {
-            *j = (n / (10i16.pow(i as u32)) % 10) as u8 + b"0"[0]
+            *j = (n / (10i16.pow(i as u32)) % 10) as u8 + b'0'
         }
 
         digit[1..].reverse();
@@ -289,11 +357,26 @@ impl COtoUTF8<6> for i16 {
 impl COtoUTF8<11> for i32 {
     fn coto_utf8(&self) -> [u8; 11] {
         let mut digit: [u8; 11] = [0; 11];
-        digit[0] = if self.is_negative() { b"-"[0] } else { b" "[0] };
+        digit[0] = if self.is_negative() { b'-' } else { b' ' };
         let n = self.abs();
 
         for (i, j) in digit[1..].iter_mut().enumerate() {
-            *j = (n / (10i32.pow(i as u32)) % 10) as u8 + b"0"[0]
+            *j = (n / (10i32.pow(i as u32)) % 10) as u8 + b'0'
+        }
+
+        digit[1..].reverse();
+
+        digit
+    }
+}
+impl COtoUTF8<20> for i64 {
+    fn coto_utf8(&self) -> [u8; 20] {
+        let mut digit = [0u8; 20];
+        digit[0] = if self.is_negative() { b'-' } else { b' ' };
+        let n = self.abs();
+
+        for (i, j) in digit[1..].iter_mut().enumerate() {
+            *j = (n / (10i64.pow(i as u32)) % 10) as u8 + b'0'
         }
 
         digit[1..].reverse();
@@ -308,7 +391,7 @@ impl COtoUTF8<10> for u32 {
         let n = *self;
 
         for (i, j) in digit.iter_mut().enumerate() {
-            *j = (n / (10u32.pow(i as u32)) % 10) as u8 + b"0"[0]
+            *j = (n / (10u32.pow(i as u32)) % 10) as u8 + b'0'
         }
 
         digit.reverse();
@@ -322,7 +405,7 @@ impl COtoUTF8<5> for u16 {
         let n = *self;
 
         for (i, j) in digit.iter_mut().enumerate() {
-            *j = (n / (10u16.pow(i as u32)) % 10) as u8 + b"0"[0]
+            *j = (n / (10u16.pow(i as u32)) % 10) as u8 + b'0'
         }
 
         digit.reverse();
